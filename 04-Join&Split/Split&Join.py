cc42e57163f2ad9af7cb9ffdd465326ff6dd0eb9
Ahmedaltu <76178825+Ahmedaltu@users.noreply.github.com>
2022-06-06 22:26:25 +0300

    Add files via upload

---
+++ 04-Join&Split/Split&Join.py
@@ -0,0 +1,32 @@
+
+def my_split(string, sep):
+    list1 = []
+    word = ''
+
+    for c in string:
+        if c not in sep:
+            word += c
+        elif word:
+            list1.append(word)
+            word = ''
+    if word:
+        list1.append(word)
+
+    return list1
+
+
+def my_join(lst, sep):
+    str = ''
+
+    for item in lst:
+        str = str + item + sep
+    str = str[:-1]
+    return str
+
+
+sentence = str(input("Please enter sentence: "))
+print(my_join(my_split(sentence, ' '), ','))
+print(my_join(my_split(sentence, ' '), '\n'))
+
+
+
